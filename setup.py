cec498e4944a617311b941b1275840963784a83b
David Warshaw <dave@cobrain.com>
2017-03-20 13:29:25 -0400

    Bump version number for release.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from setuptools import setup
 
 setup(name='hmc',
-      version='0.1',
+      version='0.2',
       description='Decision tree based hierachical multi-classifier',
       url='https://github.com/davidwarshaw/hmc',
       author='David Warshaw',
